13ab443151bc7680de6f344f326f46ea62571bf1
Naviya4 <4navindu@gmail.com>
2024-12-01 14:22:50 +0530

    Committed_4

--- src/Reverse_String/Approach_1.java
+++ src/Reverse_String/Approach_1.java
@@ -14,7 +14,7 @@ public class Approach_1 {
 }
 
 // You loop through each character of the string and add it to the beginning of the rv string.
-// This approach works but is less efficient due to the string concatenation inside the loop.
+// This approach works But it's less efficient due to the string concatenation inside the loop.
 // Each time you concatenate a string in Java, a new string object is created because strings are immutable.
 
 // Your solution uses string concatenation, which has an O(n^2) time complexity because of the way strings are
