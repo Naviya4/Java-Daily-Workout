5e422b8866c9011d1a8a5c3b6135b6d0cb6a2b4c
Naviya4 <4navindu@gmail.com>
2024-12-01 12:00:19 +0530

    Committed_2

---
+++ src/Reverse_String/Approach_1.java
@@ -0,0 +1,18 @@
+package Reverse_String;
+
+public class Approach_1 {
+    public static void main(String[] args) {
+        String st = "Hello";
+        String rv = "";
+
+        for (int i = 0; i < st.length(); i++) {
+            rv = st.charAt(i) + rv;
+        }
+
+        System.out.println(rv);
+    }
+}
+
+// You loop through each character of the string and add it to the beginning of the rv string.
+// This approach works but is less efficient due to the string concatenation inside the loop.
+// Each time you concatenate a string in Java, a new string object is created because strings are immutable.
