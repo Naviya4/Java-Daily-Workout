985aca737cd74e190c46fe2aa596d099fe5b39f1
Naviya4 <4navindu@gmail.com>
2024-12-01 12:18:36 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Reverse_String/Approach_1.java

--- src/Reverse_String/Approach_1.java
+++ src/Reverse_String/Approach_1.java
@@ -16,3 +16,6 @@ public class Approach_1 {
 // You loop through each character of the string and add it to the beginning of the rv string.
 // This approach works but is less efficient due to the string concatenation inside the loop.
 // Each time you concatenate a string in Java, a new string object is created because strings are immutable.
+
+// Your solution uses string concatenation, which has an O(n^2) time complexity because of the way strings are
+// handled in Java (new objects are created every time you concatenate).
